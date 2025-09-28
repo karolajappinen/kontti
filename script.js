97e34241b86c9a2024e547118c3f68b0e5499001
karolajappinen <karola.jappinen@gmail.com>
2025-09-28 19:11:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("clickMe").addEventListener("click", function () {
-  document.getElementById("message").textContent = "You clicked the button! 🎉";
+  document.getElementById("message").textContent = "Vittu toimmiiko?!!! Saatana!";
 });
